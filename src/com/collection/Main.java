01e5f00d9b765e45640bcf8a249505e0c72a046a
Natalis <natalis200623@gmail.com>
2021-03-07 19:08:26 +0300

    Commit

---
+++ src/com/collection/Main.java
@@ -0,0 +1,10 @@
+package com.collection;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+            for (int i = 0; i < 7; i++) {
+                System.out.println("Hello!");
+                Thread.sleep(2000);
+            }
+    }
+}
